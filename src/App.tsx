a907896a215440254e680eb6d233b1aff3cc835f
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-07-05 19:08:31 +0530

    TodoList component props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import TodoList from './components/TodoList';
+import { dummyData } from './data/todos';
+
 const App = () => {
   return (
     <>
       <main className='py-10 h-screen'>
         <h1 className='font-bold text-3xl text-center'>Your Todo</h1>
+        <div className='max-w-lg mx-auto'>
+          <div className='space-y-2'>
+            {dummyData.map((todo) => (
+              <TodoList todo={todo} />
+            ))}
+          </div>
+        </div>
       </main>
     </>
   );
---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from '../types/todo';
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: 'Like this Video 👍',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Subscribe the Channel 📺',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'And Suck your Mom 💦',
+    completed: false,
+  },
+];
